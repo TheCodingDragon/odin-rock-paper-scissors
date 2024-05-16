c4fe42d07667f39355f176d7e283f1baa8668dce
Dragon <codingdragon@proton.me>
2024-05-16 14:53:57 -0700

    Added getHumanChoice function to prompt user: 0-2 to return rock, paper or scissors. Return error for any other choice.

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
-    let choice = "Rock";
+    let choice = "rock";
 
     if (randomNumber === 1) {
-        choice = "Paper";
+        choice = "paper";
     } else if (randomNumber === 2) {
-        choice = "Scissors";
+        choice = "scissors";
     }
 
     return choice;
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    const numberChoice = parseInt(prompt("Please type 0 for rock, 1 for paper or 2 for scissors: "));
+    let choice = "error";
+
+    if (numberChoice === 0) {
+        choice = "rock";
+    } else if (numberChoice === 1) {
+        choice = "paper";
+    } else if (numberChoice === 2) {
+        choice = "scissors";
+    }
+
+    return choice;
+}
+
+console.log(getHumanChoice());
