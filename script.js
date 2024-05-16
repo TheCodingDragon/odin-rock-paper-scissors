6d59f80e92a184169629acb0290d8efbf0f7d6ac
Dragon <codingdragon@proton.me>
2024-05-16 14:36:09 -0700

    Created getComputerChoice function that uses random 0-2. Use if evaluations to return Rock, Paper or Scissors.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    let choice = "Rock";
+
+    if (randomNumber === 1) {
+        choice = "Paper";
+    } else if (randomNumber === 2) {
+        choice = "Scissors";
+    }
+
+    return choice;
+}
+
+console.log(getComputerChoice());
